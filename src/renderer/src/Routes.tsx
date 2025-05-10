2f66779dac2867acef78a299753c42937cf3d4b7
ClaudioSarmento <claudio-s-i@hotmail.com>
2025-05-09 22:39:23 -0300

    feature - Outlet

--- src/renderer/src/Routes.tsx
+++ src/renderer/src/Routes.tsx
@@ -3,17 +3,18 @@ import {Home} from './pages/home'
 import {Detail} from './pages/detail'
 import {About} from './pages/about'
 import {Create} from './pages/create'
+import {Layout} from './components/layouts'
 
 export function Routes(){
     return(
         <Router
             main={
-                <>
+                <Route path='/' element={<Layout/>}>
                     <Route path='/' element={<Home/>}/>
                     <Route path='/create' element={<Create/>}/>
                     <Route path='/about' element={<About/>}/>
                     <Route path='/detail' element={<Detail/>}/>
-                </>
+                </Route>
             }
         />
     )
---
+++ src/renderer/src/components/layouts/index.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom'
+export function Layout(){
+    return(
+        <>
+        <div className='flex-1 flex flex-col max-h-screen'>
+            <h1>TESTE LAYOUT</h1>
+            <Outlet/>
+        </div>
+        </>
+    )
+}

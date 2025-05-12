a754d0eb071ad8d0ba01c50f4586d627db0cf689
claudio <claudio-s-i@hotmail.com>
2025-05-12 16:50:22 -0300

    feature - criando layout da página home

--- src/renderer/src/pages/home.tsx
+++ src/renderer/src/pages/home.tsx
@@ -1,17 +1,51 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { Link } from "react-router-dom";
 
-export function Home(){
-    async function handleAdd(){
+export function Home() {
+    const queryClient = useQueryClient();
+
+    // Buscar os clientes
+    const { data } = useQuery({queryKey: ["customers"], queryFn: async () => {
         const response = await window.api.fethAllCustomers();
-        console.log(response);
-    }
+        return response;
+    }})
 
+    // async function handleAdd() {
+    //     const response = await window.api.fethAllCustomers();
+    //     console.log(response);
+    // }
+
+    // async function handleDeleteCustomer() {
+    //     const response = await window.api.deleteCustomer("")
+    // }
+
+    // async function handleCustomerById() {
+    //     const response = await window.api.fetchCustomerById("")
+    // }
     return (
-        <div>
-            <h1>Página Home!!!</h1>
-            <br/><br/>
-            <button onClick={handleAdd}>
-                BUSCAR USUARIOS
-            </button>
+        <div className="flex-1 flex flex-col py-12 text-white">
+            <div className="px-10">
+                <h1 className="text-white text-xl lg:text-3xl font-semibold mb-4">
+                    Todos clientes
+                </h1>
+            </div>
+            <section className="flex flex-col gap-6 w-full h-screen overflow-y-auto px-10 pb-[200px]">
+                {data?.map((customer) => (
+                    <Link 
+                        to="/" 
+                        key={customer._id}
+                        className="bg-gray-800 px-4 py-3 rounded"
+                    >
+                        <p className="mb-2 font-semibold tet-lg">{customer.name}</p>
+                        <p><span className="font-semibold">Email: </span>{customer.email}</p>
+                        {customer.phone && (
+                            <p>
+                                <span className="font-semibold">Telefone: </span> {customer.phone}
+                            </p>
+                        )}
+                    </Link>
+                ))}
+            </section>
         </div>
     )
 }
